8ef1c30882c74343e948b7fc6247cf7248cfe22a
denis-vivies-home <denis-vivies@netcourrier.com>
2018-10-12 20:55:24 +0200

    :card_file_box:change schema perm in db

--- migrations/01.sql
+++ migrations/01.sql
@@ -1,8 +1,14 @@
 CREATE SCHEMA IF NOT EXISTS hose;
 
-SET search_path TO hose;
+-- sudo -u postgres create user hose
+ALTER USER hose SET search_path TO public, hose;
 
-CREATE TABLE IF NOT EXISTS hose.user (
+DROP TABLE IF EXISTS hose.content;
+DROP TABLE IF EXISTS hose.content_type;
+DROP TABLE IF EXISTS hose.hose;
+DROP TABLE IF EXISTS hose.hose_user;
+
+CREATE TABLE hose.hose_user (
   id_user SERIAL PRIMARY KEY,
   name VARCHAR(256) UNIQUE,
   email TEXT UNIQUE,
@@ -10,23 +16,23 @@ CREATE TABLE IF NOT EXISTS hose.user (
   date_joined TIMESTAMP WITHOUT TIME ZONE DEFAULT now()
 );
 
-CREATE TABLE IF NOT EXISTS hose.hose (
+CREATE TABLE hose.hose (
   id_hose SERIAL PRIMARY KEY,
-  id_user_a BIGINT REFERENCES hose.user(id_user),
-  id_user_b BIGINT REFERENCES hose.user(id_user),
+  id_user_a BIGINT REFERENCES hose.hose_user(id_user),
+  id_user_b BIGINT REFERENCES hose.hose_user(id_user),
   date_created TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
   CHECK (id_user_a < id_user_b)
 );
 
-CREATE TABLE IF NOT EXISTS hose.content_type (
+CREATE TABLE hose.content_type (
   id_content_type SERIAL PRIMARY KEY,
   name TEXT
 );
 
-CREATE TABLE IF NOT EXISTS hose.content (
+CREATE TABLE hose.content (
   id_content SERIAL PRIMARY KEY,
   id_hose BIGINT REFERENCES hose.hose(id_hose),
-  id_user_added BIGINT REFERENCES hose.user(id_user),
+  id_user_added BIGINT REFERENCES hose.hose_user(id_user),
   date_added TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
   source_path TEXT,
   id_content_type BIGINT REFERENCES content_type(id_content_type)
